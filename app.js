abd4d5b3eb13cc211681d6f824519126223777ce
shreebaddi <74357250+shreebaddi@users.noreply.github.com>
2020-12-01 16:40:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const $climate=document.getElementById("climate");
+const $consumption=document.getElementById("consumption");
+const $resources=document.getElementById("resources");
+const $people=document.getElementById("people");
+
+
+
+$climate.onclick=()=>{
+   // console.log("climate button is working")
+   document.body.style.background="url('https://i1.faceprep.in/prograd-junior/climate-change-bg.png')";
+   $climate.style.backgroundColor="transprant";
+   $consumption.style.backgroundColor="#2F80ED";
+   $resources.style.backgroundColor="transparent";
+   $people.style.backgroundColor="transparent";
+   document.body.style.backgroundSize = "100% 100%";
+
+document.getElementById("frame").src="https://www.theworldcounts.com/embed/challenges/23?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+document.getElementById("save").innerHTML="Do your bit! Save trees, use renewable energy sources and prefer to travel green";
+}
+
+$consumption.onclick=()=>{
+    // console.log("climate button is working")
+    document.body.style.background="url('https://i1.faceprep.in/prograd-junior/food-wastage-bg.png')";
+    $climate.style.backgroundColor="transprant";
+    $consumption.style.backgroundColor="#27AE60";
+    $resources.style.backgroundColor="transparent";
+    $people.style.backgroundColor="transparent";
+    document.body.style.backgroundSize = "100% 100%";
+
+ document.getElementById("frame").src="https://www.theworldcounts.com/embed/challenges/107?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+ document.getElementById("save").innerHTML="Do your bit! Shop only what you need,eat only what you need and always save the left overs.";
+ }
+
+ $resources.onclick=()=>{
+    // console.log("climate button is working")
+    document.body.style.background="url('https://i1.faceprep.in/prograd-junior/natural-resources-bg.png')";
+    $climate.style.backgroundColor="transprant";
+    $consumption.style.backgroundColor="#F2994A";
+    $resources.style.backgroundColor="transparent";
+    $people.style.backgroundColor="transparent";
+    document.body.style.backgroundSize = "100% 100%";
+
+ document.getElementById("frame").src="https://www.theworldcounts.com/embed/challenges/5?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+ document.getElementById("save").innerHTML="Do your bit! Avoid all things plastic. Believe in Reduce, Reuse and Recycle";
+ }
+
+ $people.onclick=()=>{
+    // console.log("climate button is working")
+    document.body.style.background="url('https://i1.faceprep.in/prograd-junior/poverty-bg.png')";
+    $climate.style.backgroundColor="transprant";
+    $consumption.style.backgroundColor="#EB5757";
+    $resources.style.backgroundColor="transperant";
+    $people.style.backgroundColor="transparent";
+    document.body.style.backgroundSize = "100% 100%";
+
+ document.getElementById("frame").src="https://www.theworldcounts.com/embed/challenges/2?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+ document.getElementById("save").innerHTML="Do your bit! Never waste  food.Rather offer it to people or animals who are in need";
+ }
